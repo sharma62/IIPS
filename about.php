aa2c03143e48619d36da1ae911dfc5704e266a37
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2022-10-16 00:42:04 +0530

    commit header.php & about.php

---
+++ about.php
@@ -0,0 +1,242 @@
+<?php include "header.php" ?>
+<div class="container bg-light">
+    <h1 class="text-center py-4">About us </h1><hr>
+    
+    <!--start wrapper-->
+    <section class="wrapper">
+        <section class="page_head">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <div class="page_title">
+                            <h2>About Us</h2>
+                        </div>
+                        <nav id="breadcrumbs">
+                            <ul>
+                                <li><a href="about.html">About us</a>/</li>
+                                <li>Pages</li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="content about">
+            <div class="container">
+                <div class="row sub_content">
+                    <div class="who">
+                        <div class="col-lg-8 col-md-8 col-sm-8">
+                            <div class="dividerHeading">
+                                <h4><span>Who we are?</span></h4>
+                            </div>
+                            <img class="left_img img-thumbnail" src="images/about_1.png" alt="about img">
+                            <p>Veos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+                                voluptatem. Ut enim ad minima veniam, quis nostrum. </p>
+                            <p>Donec rutrum erat non arcu gravida porttitor. Nunc et magna nisi. Lore aliquam at erat in lorem purus aliquet mollis. Fusce elementum velit vel dolor iaculis egestas. Maecenas ut nulla quis eros scelerisque posuere vel vitae
+                                nibh eros scelerisque. </p>
+                            <p>Fusce lacinia tempor malesuada. Ut lacus sapien, placerat a ornare nec, elementum sit amet felis. Maecenas pretium lorem hendrerit eros sagittis fermentum. Donec in ut odio libero, at vulputate urna. Nulla tristique mi a massa
+                                convallis cursus. Nulla eu mi magna. Etiam suscipit commodo ad gravida. Cras suscipit, quam vitae adipiscing faucibus, risus nibh laoreet odio, a porttitor metus.</p>
+
+                        </div>
+
+                        <div class="col-lg-4 col-md-4 col-sm-4">
+                            <div class="dividerHeading">
+                                <h4><span>Our Skills</span></h4>
+                            </div>
+                            <p>Nunc et magna nisi. lore Aliquam at erat in lorem purus aliquet mollis. Fusce elementum velit vel dolor iaculis. </p>
+                            <ul class="progress-skill-bar">
+                                <li>
+                                    <span class="lable">70%</span>
+                                    <div class="progress_skill">
+                                        <div class="bar" data-value="70" role="progressbar" data-height="100">
+                                            HTML
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <span class="lable">80%</span>
+                                    <div class="progress_skill">
+                                        <div class="bar" data-value="80" role="progressbar" data-height="100">
+                                            CSS
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <span class="lable">90%</span>
+                                    <div class="progress_skill">
+                                        <div class="bar" data-value="90" role="progressbar" data-height="100">
+                                            JavaScript
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <span class="lable">80%</span>
+                                    <div class="progress_skill">
+                                        <div class="bar" data-value="80" role="progressbar" data-height="100">
+                                            MySQL
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <span class="lable">70%</span>
+                                    <div class="progress_skill">
+                                        <div class="bar" data-value="70" role="progressbar" data-height="100">
+                                            PHP
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row sub_content">
+                    <div class="col-lg-6 col-md-6 col-sm-6">
+                        <div class="dividerHeading">
+                            <h4><span>Why Choose Us?</span></h4>
+
+                        </div>
+                        <ul class="list_style circle">
+                            <li><a href="#"> Donec convallis, metus nec tempus aliquet</a></li>
+                            <li><a href="#"> Aenean commodo ligula eget dolor</a></li>
+                            <li><a href="#"> Cum sociis natoque penatibus mag dis parturient</a></li>
+                            <li><a href="#"> Lorem ipsum dolor sit amet cons adipiscing</a></li>
+                            <li><a href="#"> Accumsan vulputate faucibus turpis tortor dictum</a></li>
+                            <li><a href="#"> Nullam ultrices eros accumsan vulputate faucibus</a></li>
+                            <li><a href="#"> Nunc aliquet tincidunt metus sit amet</a></li>
+                        </ul>
+                    </div>
+
+                    <!-- TESTIMONIALS -->
+                    <div class="col-lg-6 col-md-6 col-sm-6">
+                        <div class="dividerHeading">
+                            <h4><span>What Client's Say</span></h4>
+
+                        </div>
+                        <div id="testimonial-carousel" class="testimonial carousel slide">
+                            <div class="carousel-inner">
+                                <div class="active item">
+                                    <div class="testimonial-item">
+                                        <div class="icon"><i class="fa fa-quote-right"></i></div>
+                                        <blockquote>
+                                            <p>Donec convallis, metus nec tempus aliquet, nunc metus adipiscing leo, a lobortis nisi dui ut odio. Nullam ultrices, eros accumsan vulputate faucibus, turpis tortor dictum.</p>
+                                        </blockquote>
+                                        <div class="icon-tr"></div>
+                                        <div class="testimonial-review">
+                                            <img src="images/testimonials/1.png" alt="testimoni">
+                                            <h1>Jonathan Dower,<small>Company Inc.</small></h1>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="item">
+                                    <div class="testimonial-item">
+                                        <div class="icon"><i class="fa fa-quote-right"></i></div>
+                                        <blockquote>
+                                            <p>Nunc aliquet tincidunt metus, sit amet mattis lectus sodales ac. Suspendisse rhoncus dictum eros, ut egestas eros luctus eget. Nam nibh sem, mattis et feugiat ut, porttitor nec risus.</p>
+                                        </blockquote>
+                                        <div class="icon-tr"></div>
+                                        <div class="testimonial-review">
+                                            <img src="images/testimonials/2.png" alt="testimoni">
+                                            <h1>Jonathan Dower<small>Leopard</small></h1>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="testimonial-buttons"><a href="#testimonial-carousel" data-slide="prev"><i class="fa fa-chevron-left"></i></a>&#32;
+                                <a href="#testimonial-carousel" data-slide="next"><i class="fa fa-chevron-right"></i></a></div>
+                        </div>
+                    </div>
+                    <!-- TESTIMONIALS END -->
+                </div>
+
+<!-- 
+                <div class="row  sub_content">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <div class="dividerHeading">
+                            <h4><span>Meet the Team</span></h4>
+
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+                        <div class="our-team">
+                            <div class="pic">
+                                <img src="images/teams/1.png" alt="profile img">
+                                <div class="social_media_team">
+                                    <ul class="team_social">
+                                        <li><a class="fb" href="#." data-placement="top" data-toggle="tooltip" title="Facbook"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a class="twtr" href="#." data-placement="top" data-toggle="tooltip" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a class="gmail" href="#." data-placement="top" data-toggle="tooltip" title="Google"><i class="fa fa-google-plus"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="team_prof">
+                                <h3 class="post-title">kristiana<small>Web Developer</small></h3>
+                                <p class="description">Phasellus ac libero ac tellus pellentesque semper. Sed ac felis. Sed commo, magnase quis lacinia ornare, quam ante aliqua nisi, eu iaculis leo purus venenatis scelerisque. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+                        <div class="our-team">
+                            <div class="pic">
+                                <img src="images/teams/2.png" alt="profile img">
+                                <div class="social_media_team">
+                                    <ul class="team_social">
+                                        <li><a class="fb" href="#." data-placement="top" data-toggle="tooltip" title="Facbook"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a class="twtr" href="#." data-placement="top" data-toggle="tooltip" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a class="gmail" href="#." data-placement="top" data-toggle="tooltip" title="Google"><i class="fa fa-google-plus"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="team_prof">
+                                <h3 class="post-title">williamson<small>Web Developer</small></h3>
+                                <p class="description">Phasellus ac libero ac tellus pellentesque semper. Sed ac felis. Sed commo, magnase quis lacinia ornare, quam ante aliqua nisi, eu iaculis leo purus venenatis scelerisque. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+                        <div class="our-team">
+                            <div class="pic">
+                                <img src="images/teams/3.png" alt="profile img">
+                                <div class="social_media_team">
+                                    <ul class="team_social">
+                                        <li><a class="fb" href="#." data-placement="top" data-toggle="tooltip" title="Facbook"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a class="twtr" href="#." data-placement="top" data-toggle="tooltip" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a class="gmail" href="#." data-placement="top" data-toggle="tooltip" title="Google"><i class="fa fa-google-plus"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="team_prof">
+                                <h3 class="post-title">miranda joy<small>Web Desginer</small></h3>
+                                <p class="description">Phasellus ac libero ac tellus pellentesque semper. Sed ac felis. Sed commo, magnase quis lacinia ornare, quam ante aliqua nisi, eu iaculis leo purus venenatis scelerisque. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+                        <div class="our-team">
+                            <div class="pic">
+                                <img src="images/teams/4.png" alt="profile img">
+                                <div class="social_media_team">
+                                    <ul class="team_social">
+                                        <li><a class="fb" href="#." data-placement="top" data-toggle="tooltip" title="Facbook"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a class="twtr" href="#." data-placement="top" data-toggle="tooltip" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a class="gmail" href="#." data-placement="top" data-toggle="tooltip" title="Google"><i class="fa fa-google-plus"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="team_prof">
+                                <h3 class="post-title">steve thomas<small>Web Developer</small></h3>
+                                <p class="description">Phasellus ac libero ac tellus pellentesque semper. Sed ac felis. Sed commo, magnase quis lacinia ornare, quam ante aliqua nisi, eu iaculis leo purus venenatis scelerisque. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+            </div>
+        </section>
+    </section>
+    <!--end wrapper-->
+    
+</div>
+<?php include "footer.php" ?>
---
+++ header.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<?php
+include('constant.inc.php ');
+include('database.inc.php');
+include('function.inc.php');
+?>
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title><?php echo  'IIPS -' . strtoupper($title[0]); ?></title>
+    <meta name="description" content="">
+    <!-- font awsome -->
+    <script src="https://kit.fontawesome.com/43aded160e.js" crossorigin="anonymous"></script>
+
+    <link rel="icon" href="favicon.png" type="image/x-icon">
+
+    <!-- CSS FILES -->
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/iips_stylesheet.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen" data-name="skins">
+    <link rel="stylesheet" href="css/layout/wide.css" data-name="layout">
+    <link rel="stylesheet" href="css/new_style.css">
+
+   
+    <link rel="stylesheet" href="css/style-fraction.css" />
+    <link rel="stylesheet" href="css/mediaQuery.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+    <style>
+        .sidenav {
+            height: 100%;
+            width: 0;
+            position: fixed;
+            z-index: 1;
+            top: 0;
+            left: 0;
+            background-color: #111;
+            overflow-x: hidden;
+            transition: 0.5s;
+            padding-top: 60px;
+            text-align: center;
+        }
+
+        .sidenav a {
+            padding: 8px 8px 8px 32px;
+            text-decoration: none;
+            font-size: 25px;
+            color: #818181;
+            display: block;
+            transition: 0.3s;
+        }
+
+        .sidenav a:hover {
+            color: #f1f1f1;
+        }
+
+        .sidenav .closebtn {
+            position: absolute;
+            top: 0;
+            right: 25px;
+            font-size: 36px;
+            margin-left: 50px;
+        }
+
+        @media screen and (max-height: 450px) {
+            .sidenav {
+                padding-top: 15px;
+            }
+
+            .sidenav a {
+                font-size: 18px;
+            }
+        }
+
+
+
+
+        
+
+        
+    </style>
+</head>
+<!-- navigation bar -->
+<body class="home school animate-bottom">
+<header id="header">
+    <div id="top-bar">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 top-info hidden-xs">
+                    <span><i class="fa fa-phone"></i>Phone: (123) 456-7890</span>
+                    <span><i class="fa fa-envelope"></i>Email: mail@example.com</span>
+                </div>
+                <div class="col-sm-4 top-info">
+                    <ul>
+                        <li><a href="" class="my-tweet"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="" class="my-facebook"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="" class="my-skype"><i class="fa fa-skype"></i></a></li>
+                        <li><a href="" class="my-pint"><i class="fa fa-pinterest"></i></a></li>
+                        <li><a href="" class="my-rss"><i class="fa fa-rss"></i></a></li>
+                        <li><a href="" class="my-google"><i class="fa fa-google-plus"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col offset-2">
+            <div id="mySidenav" class="sidenav">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href="<?php echo 'about.php' ?>">About</a>
+                <a href="<?php echo 'registeration.php' ?>">Registeration</a>
+                <a href="<?php echo 'imgGallary.php' ?>">Image Gallary</a>
+                <a href="<?php echo 'faculty.php' ?>">Faculty</a>
+                <a href="#">Student</a>
+                <a href="#">Examnation</a>
+                <a href="#">Contact</a>
+            </div>
+            <br>
+            <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; </span>
+
+            <script>
+                function openNav() {
+                    document.getElementById("mySidenav").style.width = "360px";
+
+                }
+
+                function closeNav() {
+                    document.getElementById("mySidenav").style.width = "0";
+                }
+            </script>
+
+        </div>
+        <div class="col-lg-5 col-md-4 hidden-sm hidden-xs">
+            <img src="assets//img//azadi-logo.png" alt="" width="120px">
+           <a target="_blank" href="https://www.digitalindia.gov.in/"><img src="assets//img//digital-india.png" alt="" width="120px"></a> 
+            <a target="_blank" href="https://swachhbharat.mygov.in/"><img src="assets//img//swach-bharat.jpg" alt="" width="120px"></a>
+        </div>
+        
+        <div class="col">
+            <a href="index.php">
+                <img src="iips_logo.jpg" alt="logo" width="35%" style="border-radius: 100px;" id="main-logo">
+            </a>
+        </div>
+    </div>
+</div>
+<section class="container-fluid ">
+
+    <div class="topnav" id="myTopnav">
+        <a href="#home" class="active">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div></section>
+
+
+
+    <script> 
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
+
+    document.body.className="animate-bottom";
+ </script>
+
+</section>
