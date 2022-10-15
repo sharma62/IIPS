b72ba15b9b815861e568cd3cef6a69fd540fd982
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2022-10-16 00:43:40 +0530

    add footer page

---
+++ footer.php
@@ -0,0 +1,104 @@
+ 
+    <!--start footer-->
+    <footer class="footer ">
+        <div class="container ">
+            <div class="row ">
+                <div class="col-sm-6 col-md-3 col-lg-3 ">
+                    <div class="widget_title ">
+                        <h4><span>About Us</span></h4>
+                    </div>
+                    <div class="widget_content ">
+                        <ul class="contact-details-alt ">
+                            <li><i class="fa fa-map-marker "></i>
+                                <p><strong>Address</strong>: Bihta , Amhara</p>
+                            </li>
+                            <li><i class="fa fa-user "></i>
+                                <p><strong>Phone</strong>:(+91) 9000-12345</p>
+                            </li>
+                            <li><i class="fa fa-envelope "></i>
+                                <p><strong>Email</strong>: <a href="# ">IIps@gmail.com</a></p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 ">
+                    <div class="widget_title ">
+                        <h4><span>Recent Posts</span></h4>
+                    </div>
+                    <div class="widget_content ">
+                        <ul class="links ">
+                            <li><a href="# ">link-A<span>November 07, 2020</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 ">
+                    <div class="widget_title ">
+                        <h4><span>Twitter</span></h4>
+                    </div>
+                    <div class="widget_content ">
+                        <ul class="tweet_list ">
+                            <li class="tweet_content item ">
+                                <p class="tweet_link "><a href="# ">@demopoint </a> log in & check that</p>
+                                <span class="time ">29 September 2020</span>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <div class="widget_content ">
+                        <div class="tweet_go "></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 ">
+                    <div class="widget_title ">
+                        <h4><span>short NOTE</span></h4>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+    <section class="footer_bottom ">
+        <div class="container ">
+            <div class="row ">
+                <div class="col-sm-6 ">
+                    <p class="copyright ">&copy; Copyright 2020 IIPS | Powered by <a href="https://github.com/sharma62">Sharma62</a></p>
+                </div>
+
+                <div class="col-sm-6 ">
+                    <div class="footer_social ">
+                        <ul class="footbot_social ">
+                            <li><a class="fb " href="#. " data-placement="top " data-toggle="tooltip " title="Facebook "><i class="fa fa-facebook "></i></a></li>
+                            <li><a class="twtr " href="#. " data-placement="top " data-toggle="tooltip " title="Twitter "><i class="fa fa-twitter "></i></a></li>
+                            <li><a class="dribbble " href="#. " data-placement="top " data-toggle="tooltip " title="Dribbble "><i class="fa fa-dribbble "></i></a></li>
+                            <li><a class="skype " href="#. " data-placement="top " data-toggle="tooltip " title="Skype "><i class="fa fa-skype "></i></a></li>
+                            <li><a class="rss " href="#. " data-placement="top " data-toggle="tooltip " title="RSS "><i
+                                        class="fa fa-rss "></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <!--end footer-->
+
+    <script type="text/javascript " src="js/jquery-1.10.2.min.js "></script>
+    <script src="js/bootstrap.min.js "></script>
+
+    <script type="text/javascript " src="js/jquery.cookie.js "></script>
+    <!-- jQuery cookie -->
+    <script type="text/javascript " src="js/jquery.smartmenus.min.js "></script>
+    <script type="text/javascript " src="js/jquery.smartmenus.bootstrap.min.js "></script>
+    <script type="text/javascript " src="js/jquery.magnific-popup.min.js "></script>
+    <script type="text/javascript " src="js/jquery.isotope.min.js "></script>
+    <script type="text/javascript " src="js/jquery.easypiechart.min.js "></script>
+    <script type="text/javascript " src="js/jquery-hoverdirection.min.js "></script>
+    <script type="text/javascript " src="js/jquery.matchHeight-min.js "></script>
+    <script type="text/javascript " src="js/jquery-scrolltofixed-min.js "></script>
+    <script src="js/main.js "></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+   
+</body>
+
+</html>
